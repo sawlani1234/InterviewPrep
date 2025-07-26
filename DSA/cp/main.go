c18039a47f1503e2813f80ef2c0e68e7736fbb57
SHUBHAM SAWLANI <shubhamsawlani@SHUBHAMs-MacBook-Air.local>
2025-07-26 13:17:25 +0530

    added CP file

---
+++ DSA/cp/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// template for input
+func main() {
+	var a,b,c int 
+	var s string
+	
+	fmt.Scanf("%d",&a)
+	fmt.Scanf("%d %d",&b,&c)
+	fmt.Scanf("%s",&s)
+
+	fmt.Println(a+b+c, " ",s)
+
+}
